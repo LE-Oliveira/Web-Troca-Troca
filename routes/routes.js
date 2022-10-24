a0b689cc9388225e4784fd7555d6800b758647af
Perna <aber00fort@gmail.com>
2022-10-24 17:59:14 -0300

    Criacao do album ao registrar-se

--- routes/routes.js
+++ routes/routes.js
@@ -21,6 +21,14 @@ router.post('/register', jsonParser, async (req, res)=>{
                 password: req.body.password
                 });
             const dataToSave = await user.save();
+            for(var i=0;i<447;i++){
+                var figs = new StickerPerson({
+                    username: name,
+                    fidSticker: i+1,
+                    option: "need"
+                })
+                const dataToSave = await figs.save();
+            }
             res.status(200).json(dataToSave);
         }
         else{
@@ -66,7 +74,8 @@ router.delete('/deletePerson', async (req, res)=>{
     try{
         const id = req.header('id');
         const data = await Person.findByIdAndDelete(id);
-        res.send(`Document with ${data.name} has been deleted`);
+        const c = await StickerPerson.deleteMany({username: data.username});
+        res.send(`Usuário ${data.username} foi deletado com as relações ${c}`);
     }
     catch(error){
         res.status(400).json({message: error.message})
